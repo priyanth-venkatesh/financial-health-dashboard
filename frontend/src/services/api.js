64f48bf21f3f9f69ce5cf3364b92ab05b6dddeb0
Karthik Priyanth <karthikpriyanth@Karthiks-Laptop.local>
2026-02-06 20:03:08 +0530

    fix: connect frontend to deployed backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:8000",
-});
+    baseURL: "https://finance-backend-lmao.onrender.com"
+  });  
 
 API.interceptors.request.use((config) => {
   const token = localStorage.getItem("token");

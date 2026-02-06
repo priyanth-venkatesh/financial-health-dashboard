eff31cc56f1b7ea8c492d39be25a82089ea49ac7
Karthik Priyanth <karthikpriyanth@Karthiks-Laptop.local>
2026-02-06 19:14:50 +0530

    fix: use neon database url

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
-DATABASE_URL = "postgresql://karthikpriyanth@localhost/finance"
-
+DATABASE_URL = os.getenv("DATABASE_URL")
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
